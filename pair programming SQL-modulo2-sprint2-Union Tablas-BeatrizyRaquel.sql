bae5355d0712736bcc78ece212fbb33053102590
Beatriz <b.sanchez.reguera6@gmail.com>
2024-03-02 00:12:51 +0100

    Add new pair programming SQL Joins

---
+++ pair programming SQL-modulo2-sprint2-Union Tablas-BeatrizyRaquel.sql
@@ -0,0 +1,43 @@
+-- Pair programming -- SQL - Union Tablas (Joins)
+-- Ejercicios
+
+-- 1. Pedidos por empresa en UK
+SELECT 
+FROM
+
+-- 2. Productos pedidos por empresa en UK por año
+SELECT 
+FROM
+
+-- 3. Mejorad la query anterior
+SELECT 
+FROM
+
+-- 4. BONUS: Pedidos que han realizado cada compañía y su fecha
+SELECT 
+FROM
+
+-- 5. BONUS: Tipos de producto vendidos
+SELECT 
+FROM
+
+-- 6. Qué empresas tenemos en la BBDD Northwind
+SELECT 
+FROM
+
+-- 7. Pedidos por cliente de UK
+SELECT 
+FROM
+
+-- 8. Empresas de UK y sus pedidos
+SELECT 
+FROM
+
+-- 9. Empleadas que sean de la misma ciudad
+SELECT 
+FROM
+
+-- 10. BONUS: FULL OUTER JOIN Pedidos y empresas con pedidos asociados o no
+SELECT 
+FROM
+
