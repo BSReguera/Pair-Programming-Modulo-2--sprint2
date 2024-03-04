488bf03d8e7a2f808c67b50fec4da9bce67333b1
Beatriz <b.sanchez.reguera6@gmail.com>
2024-03-04 22:11:33 +0100

    Updated SQL

--- pair programming SQL-modulo2-sprint2-Subconsultas-BeatrizyRaquel.sql
+++ pair programming SQL-modulo2-sprint2-Subconsultas-BeatrizyRaquel.sql
@@ -1,20 +1,29 @@
 -- Pair programming -- SQL - Subconsultas
 
 -- 0. Observamos las tablas a usar
-SELECT  
-	FROM 
-SELECT  
-	FROM 
+SELECT *
+	FROM ``;
+SELECT  *
+	FROM ``;
+    
 -- 1. Extraed los pedidos con el máximo "order_date" para cada empleado.
-
+SELECT `order_id`, `customer_id`, `employye_id`, `order_date`, `required_date`
+	FROM 
 -- 2. Extraed el precio unitario máximo (unit_price) de cada producto vendido.
-
+SELECT 
+	FROM
 -- 3. Extraed información de los productos "Beverages"
-
+SELECT 
+	FROM
 -- 4. Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país
-
+SELECT 
+	FROM
 -- 5. Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"
-
+SELECT 
+	FROM
 -- 6. Extraed los 10 productos más caros
-
+SELECT 
+	FROM
 -- BONUS: 7. Qué producto es más popular
+SELECT 
+	FROM
