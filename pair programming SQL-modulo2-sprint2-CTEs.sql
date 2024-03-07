6f8698ebf25683bd8d722fa55a40e911f55c2c1a
raquelmartinrodriguez <atv.raque@gmail.com>
2024-03-07 11:44:19 +0100

    ejercicios terminados, indentación corregida

--- pair programming SQL-modulo2-sprint2-CTEs.sql
+++ pair programming SQL-modulo2-sprint2-CTEs.sql
@@ -1,14 +1,4 @@
 -- Pair Programming - CTEs
-
--- 0. Ejemplo - CTE:
-WITH `media` AS (SELECT `product_line`, AVG (`buy_price`) AS `media_precio`
-					FROM `products`
-					GROUP BY `product_line`)
-                    
-SELECT `product_name`, `products`.`product_line`, `buy_price`, `media_precio`
-	FROM `products`
-    INNER JOIN `media`
-    ON `products`.`product_line` = `media`.`product_line`;
     
 -- 1. Extraer en una CTE todos los nombres de las compañias y los id de los clientes.
 WITH `clientes` AS (SELECT `customer_id`, `company_name`
@@ -26,28 +16,22 @@ SELECT *
 -- 3. Extraed el id de las facturas y su fecha de cada cliente.
 SELECT `customers`.`customer_id`, `customers`.`company_name`, `orders`.`order_id`, `orders`.`order_date`
 	FROM `customers`
-INNER JOIN `orders`
-	ON `customers`. `customer_id`= `orders`. `customer_id`;
+	INNER JOIN `orders`
+		ON `customers`. `customer_id`= `orders`. `customer_id`;
 	
 -- 4. Contad el número de facturas por cliente
 SELECT `customers`.`customer_id`, `customers`.`company_name`, COUNT(`order_id`) AS 'num_facturas'
 	FROM `customers`
-INNER JOIN `orders`
-	ON `customers`. `customer_id`= `orders`. `customer_id`
+	INNER JOIN `orders`
+		ON `customers`. `customer_id`= `orders`. `customer_id`
 	GROUP BY `customers`.`customer_id`;
 
 -- 5. Cuál la cantidad media pedida de todos los productos ProductID.
-SELECT `od`.`product_id`, AVG (`od`.`quantity`),`p`.`product_name`
-	FROM `order_details` AS `od`
-INNER JOIN `products` AS `p`
-	ON `od`.`product_id`= `p`.`product_id`
-	GROUP BY `product_id`;
-    
 SELECT `p`.`product_name`, AVG(`od`.`quantity`) AS "media"
 	FROM `order_details` AS `od`
-INNER JOIN `products` AS `p`
-    ON `od`.`product_id` = `p` .`product_id`
-GROUP BY `od`.`product_id`, `p`.`product_name`;
+	INNER JOIN `products` AS `p`
+		ON `od`.`product_id` = `p` .`product_id`
+	GROUP BY `od`.`product_id`, `p`.`product_name`;
 
 -- 6. BONUS: Usando una CTE, extraer el nombre de las diferentes categorías de productos, con su precio medio, máximo y mínimo.
 WITH `cte_category` AS (SELECT `category_id`, AVG(`unit_price`) AS `media`, MAX(`unit_price`) AS `maximo`, MIN(`unit_price`) AS `minimo`
@@ -56,8 +40,8 @@ WITH `cte_category` AS (SELECT `category_id`, AVG(`unit_price`) AS `media`, MAX(
                         
 SELECT `category_name`, `media`, `maximo`, `minimo`
 	FROM `cte_category`
-INNER JOIN `categories` AS `c`
-	ON `cte_category`.`category_id`= `c`. `category_id`;
+	INNER JOIN `categories` AS `c`
+		ON `cte_category`.`category_id`= `c`. `category_id`;
 
 -- 7. La empresa nos ha pedido que busquemos el nombre de cliente, su teléfono y el número de pedidos que ha hecho cada uno de ellos.
 WITH `cte_clientes_pedidos`AS (
@@ -66,8 +50,8 @@ WITH `cte_clientes_pedidos`AS (
 		GROUP BY `customer_id`)
 SELECT `c`.`contact_name`, `c`.`phone`,`c`.`customer_id`, `p`.`Numpedidos`
 	FROM `customers` AS `c`
-INNER JOIN `cte_clientes_pedidos`AS `p`
-	ON `c`.`customer_id` = `p`.`customer_id`; 
+	INNER JOIN `cte_clientes_pedidos`AS `p`
+		ON `c`.`customer_id` = `p`.`customer_id`; 
 
 -- 8. Modifica la consulta anterior para obtener los mismos resultados pero con los pedidos por año que ha hecho cada cliente.
 WITH `cte_clientes_pedidos`AS (
@@ -76,8 +60,8 @@ WITH `cte_clientes_pedidos`AS (
 		GROUP BY `customer_id`,YEAR(`order_date`))
 SELECT `c`.`contact_name`, `c`.`phone`,`c`.`customer_id`, `p`.`año`,`p`.`Numpedidos`
 	FROM `customers` AS `c`
-INNER JOIN `cte_clientes_pedidos`AS `p`
-ON `c`.`customer_id` = `p`.`customer_id`; 
+	INNER JOIN `cte_clientes_pedidos`AS `p`
+		ON `c`.`customer_id` = `p`.`customer_id`; 
 
 -- 9. Modifica la cte del ejercicio anterior, úsala en una subconsulta para saber el nombre del cliente y su teléfono, para aquellos clientes que hayan hecho más de 6 pedidos en el año 1998.
 WITH `cte_clientes_pedidos`AS (
@@ -87,21 +71,19 @@ WITH `cte_clientes_pedidos`AS (
     
 SELECT `c`.`contact_name`, `c`.`phone`, `p`.`año`,`p`.`Numpedidos`
 	FROM `customers` AS `c`
-INNER JOIN (
-			SELECT `customer_id`,`p`.`año`,`p`.`Numpedidos`
-				FROM `cte_clientes_pedidos`AS `p`
-				WHERE `año` = 1998 AND `Numpedidos` > 6)
-AS `p` ON `c`.`customer_id` = `p`.`customer_id`; 
+	INNER JOIN (SELECT `customer_id`,`p`.`año`,`p`.`Numpedidos`
+					FROM `cte_clientes_pedidos`AS `p`
+					WHERE `año` = 1998 AND `Numpedidos` > 6) AS `p` 
+		ON `c`.`customer_id` = `p`.`customer_id`; 
 
 -- 10 Nos piden que obtengamos el importe total (teniendo en cuenta los descuentos) de cada pedido de la tabla orders y el customer_id asociado a cada pedido.
-WITH `cte_total_amount` AS (
-								SELECT `order_id`, SUM(`unit_price` * `quantity` * (1 - `discount`)) AS "Total_amount"
-									FROM `order_details`AS `od`
-									GROUP BY `order_id`)
+WITH `cte_total_amount` AS (SELECT `order_id`, SUM(`unit_price` * `quantity` * (1 - `discount`)) AS "Total_amount"
+								FROM `order_details`AS `od`
+								GROUP BY `order_id`)
 SELECT  `o`.`order_id`, `o`.`customer_id`,`t`.`Total_amount`
 	FROM `orders`  AS `o`
 	INNER JOIN  `cte_total_amount` AS `t`
-    ON `o`.`order_id`= `t`.`order_id`;
+		ON `o`.`order_id`= `t`.`order_id`;
 
 
 
--- pair programming SQL-modulo2-sprint2-Subconsultas-BeatrizyRaquel.sql
+++ pair programming SQL-modulo2-sprint2-Subconsultas-BeatrizyRaquel.sql
@@ -10,8 +10,8 @@ SELECT  *
 SELECT `order_id`, `customer_id`, `employee_id`, `order_date`, `required_date`
 	FROM `orders` AS `o1`
     WHERE `order_date` IN (SELECT MAX(`order_date`)
-									FROM `orders` AS `o2`
-                                    WHERE  `o1`.`employee_id`= `o2`.`employee_id`);
+								FROM `orders` AS `o2`
+								WHERE  `o1`.`employee_id`= `o2`.`employee_id`);
 -- Querie de base correlacionada
 SELECT MAX(`order_date`)
 	FROM `orders` AS `o2`
@@ -21,9 +21,9 @@ SELECT MAX(`order_date`)
 SELECT `od1`.`product_id`, `od1`.`unit_price` AS "Maxunitprice"
 	FROM `order_details` AS `od1`
     WHERE `od1`.`unit_price` IN (SELECT MAX(`od2`.`unit_price`)
-					FROM `order_details` AS `od2`
-					WHERE `od1`.`product_id` = `od2`.`product_id`)
-GROUP BY `od1`.`product_id`,`od1`.`unit_price`;
+									FROM `order_details` AS `od2`
+									WHERE `od1`.`product_id` = `od2`.`product_id`)
+	GROUP BY `od1`.`product_id`,`od1`.`unit_price`;
   
 -- 3. Extraed información de los productos "Beverages"
 SELECT `product_id`, `product_name`, `category_id`
@@ -36,14 +36,14 @@ SELECT `country`
     WHERE NOT EXISTS (SELECT *
 						FROM `suppliers` AS `s`
 						WHERE `s`.`country`= `c`.`country`)
-GROUP BY `country`; 
+	GROUP BY `country`; 
     
 -- 5. Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"
 SELECT `order_id`, `customer_id`
-FROM `orders`
-WHERE `order_id` IN (SELECT `order_id`
-		FROM `order_details`
-        WHERE  `product_id` = 6 AND `quantity` > 20);
+	FROM `orders`
+	WHERE `order_id` IN (SELECT `order_id`
+							FROM `order_details`
+							WHERE  `product_id` = 6 AND `quantity` > 20);
         
 -- 6. Extraed los 10 productos más caros
 SELECT `product_name` AS 'Ten_Most_Expensive_Products' , `unit_price`
@@ -54,8 +54,8 @@ SELECT `product_name` AS 'Ten_Most_Expensive_Products' , `unit_price`
 -- BONUS: 7. Qué producto es más popular                     
 SELECT `p`.`product_name`,
     (SELECT SUM(`quantity`)
-    FROM `order_details`
-    WHERE `product_id` = `p`.`product_id`) AS "Total_quantity"
-FROM `products` AS `p`
-ORDER BY `total_quantity` DESC
-LIMIT 1;
+		FROM `order_details`
+		WHERE `product_id` = `p`.`product_id`) AS "Total_quantity"
+	FROM `products` AS `p`
+	ORDER BY `total_quantity` DESC
+	LIMIT 1;
